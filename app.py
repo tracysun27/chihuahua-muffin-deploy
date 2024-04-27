1d79c353c56e4f9486502914252ab8aba68e0817
Tracy Sun <96366527+tracysun27@users.noreply.github.com>
2024-04-27 15:25:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -40,20 +40,14 @@ def save_image(image_bytes):
 
     # making it square
     (height, width) = frame.shape[:2]
-    # if height < width:
-    #     frame = frame[
-    #         0:height, ((width // 2) - (height // 2)) : ((width // 2) + (height // 2))
-    #     ]
-    # else:
-    #     frame = frame[
-    #         ((height // 2) - (width // 2)) : ((height // 2) + (width // 2)), 0:width
-    #     ]
     if height < width:
-        start_x = (width - height) // 2
-        frame = frame[0:height, start_x:start_x + height]
+        frame = frame[
+            0:height, ((width // 2) - (height // 2)) : ((width // 2) + (height // 2))
+        ]
     else:
-        start_y = (height - width) // 2
-        frame = frame[start_y:start_y + width, 0:width]
+        frame = frame[
+            ((height // 2) - (width // 2)) : ((height // 2) + (width // 2)), 0:width
+        ]
 
     # Construct the image name
     img_name = f'captured_image_{dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.jpg'
